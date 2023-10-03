1037f8873c5cd9a9902a4fc6e15ecefb9166f34c
JBwdn <jake.bowden95@gmail.com>
2023-10-03 16:27:13 +0100

    Test scripting for package

---
+++ scripts/__init__.py
@@ -0,0 +1,1 @@
+import scripts.proxel_test
---
+++ scripts/proxel_test.py
@@ -0,0 +1,14 @@
+"""
+Simple test script to demo project setup.
+"""
+
+# import proxel
+
+
+def main():
+    """Script entry point."""
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
